53c20caca4feebc5a1d376ae4223393f8b1d43ed
Hoda <hhesham2582000@gmail.com>
2023-05-08 14:41:34 +0300

    button styles

--- Projects/GuessTheFlag/Guess The Flag/ContentView.swift
+++ Projects/GuessTheFlag/Guess The Flag/ContentView.swift
@@ -13,7 +13,13 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
-    
+    @State private var counter = 0
+    @State private var gameOver:Bool = false
+    @State private var finalScore = 0
+    @State private var youLosed = false
+    @State private var buttonScale: [CGFloat] = [1.0, 1.0, 1.0]
+    @State private var buttonOpacity: [Double] = [1.0, 1.0, 1.0]
+
     var body: some View {
         
         ZStack{
@@ -34,16 +40,27 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .font(.largeTitle.weight(.semibold))
                     }
-                    ForEach(0..<3){index in
-                        Button {
+                    ForEach(0..<3) { index in
+                        Button(action: {
                             flagTapped(index)
-                        } label: {
+                        }) {
                             Image(countries[index])
                                 .clipShape(RoundedRectangle(cornerRadius: 10))
                                 .shadow(radius: 5)
                         }
-                        
-                        
+                        .scaleEffect(buttonScale(index))
+                        .opacity(buttonOpacity(index))
+                        .animation(.easeInOut(duration: 0.1))
+                        .onTapGesture {
+                            withAnimation {
+                                buttonScale[index] = 0.95
+                                buttonOpacity[index] = 0.8
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                    buttonScale[index] = 1.0
+                                    buttonOpacity[index] = 1.0
+                                }
+                            }
+                        }
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -65,7 +82,14 @@ struct ContentView: View {
         } message: {
             Text("Your score is: \(score)")
         }
-        
+        .alert("You Won", isPresented: $gameOver){
+            Button("Play Again", action: askQuestions)
+        } message:{
+                    Text("Your score is \(finalScore) out of 8")
+            }
+        .alert("Game Over!", isPresented: $youLosed){
+            Button("Play Again", action: askQuestions)
+        }
     }
     
     
@@ -73,22 +97,39 @@ struct ContentView: View {
         if number == correctAnswer{
             scoreTitle = "Correct"
             score += 1
+            showingScore = true
+            counter += 1
         }
         else {
             if score == 0 {
-                scoreTitle = "Game Over!"
-                
+                youLosed = true
+                counter = 0
             }
             else{
-                scoreTitle = "False"
+                scoreTitle = "Wrong! \n That’s the flag of \(countries[number])"
                 score -= 1
+                showingScore = true
+                counter += 1
             }
         }
-        showingScore = true
+        
     }
     private func askQuestions (){
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        if counter == 7 {
+            gameOver = true
+            finalScore = score
+            score = 0
+        }
+        
+    }
+    func buttonScale(_ index: Int) -> CGFloat {
+        buttonScale[index]
+    }
+
+    func buttonOpacity(_ index: Int) -> Double {
+        buttonOpacity[index]
     }
     
 }
